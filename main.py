06ff05c9c168c29fa3360532addcb02e61eb7d6b
Artem <vebulogmetra@yandex.ru>
2023-10-26 15:40:02 +0300

    remove lifespan with create_all

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
 from src.core.models import Base
 from src.core.utils.database import db_helper
 
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as connect:
-        await connect.run_sync(Base.metadata.create_all)
-    yield
+#     yield
 
 
-app: FastAPI = FastAPI(lifespan=lifespan)
+app: FastAPI = FastAPI()
 
 
 @app.get("/")
