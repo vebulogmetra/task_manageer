07953b736e986a24b7b682c55f7738d23d8a628d
Artem <vebulogmetra@yandex.ru>
2023-10-27 17:16:40 +0300

    add print development, db_debug variables

--- src/core/settings/config.py
+++ src/core/settings/config.py
@@ -16,21 +16,23 @@ class Config(BaseSettings):
         env_file_encoding="utf-8",
         extra="ignore",
     )
-    api_v1_prefix: str = "/api/v1"
+
     development: bool
+    api_v1_prefix: str = "/api/v1"
     app_host: str
     app_port: int
     app_workers_count: int
+
+    db_alchemy_driver: str = "postgresql+asyncpg"
+    debug_database: bool
     db_host: str
     db_port: int
     db_user: str
     db_name: str
     db_password: str
 
-    def get_sqlalchemy_db_url(self) -> str:
-        url: str = f"postgresql+asyncpg://{self.db_user}:{self.db_password}@\
-{self.db_host}:{self.db_port}/{self.db_name}"
-        return url
-
 
 settings = Config()
+
+print(f"DEVELOPMENT: {settings.development}")
+print(f"DEBUG_DATABASE: {settings.debug_database}")
