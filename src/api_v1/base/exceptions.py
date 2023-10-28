d8eb198955540493ca030c953fc12c9be341d102
Artem <vebulogmetra@yandex.ru>
2023-10-28 16:26:04 +0300

    moved the common templates to api_v1/base

--- src/api_v1/base/exceptions.py
+++ src/api_v1/base/exceptions.py
@@ -3,3 +3,7 @@ from fastapi import HTTPException, status
 users_not_found = HTTPException(
     status_code=status.HTTP_404_NOT_FOUND, detail="User(-s) not found!"
 )
+
+tasks_not_found = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND, detail="Task(-s) not found!"
+)
