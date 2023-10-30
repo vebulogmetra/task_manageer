29341f43d146591956961906ce6c80b819a3ddbf
Artem <vebulogmetra@yandex.ru>
2023-10-30 13:28:40 +0300

    add new config items: app_title, version, db_url

--- src/core/settings/config.py
+++ src/core/settings/config.py
@@ -18,12 +18,12 @@ class Config(BaseSettings):
     )
 
     development: bool
+    app_title: str = "API for TaskManageer"
+    app_version: str = "0.1.1"
     api_v1_prefix: str = "/api/v1"
     app_host: str
     app_port: int
     app_workers_count: int
-
-    db_alchemy_driver: str = "postgresql+asyncpg"
 
     debug_database: bool
 
@@ -32,21 +32,17 @@ class Config(BaseSettings):
     db_user: str
     db_name: str
     db_password: str
+    db_alchemy_url: str
 
     test_db_host: str
     test_db_port: int
     test_db_user: str
     test_db_name: str
     test_db_password: str
+    test_db_alchemy_url: str
 
 
 settings = Config()
-
-settings.db_alchemy_url = f"{settings.db_alchemy_driver}://{settings.db_user}:{settings.db_password}@\
-{settings.db_host}:{settings.db_port}/{settings.db_name}"
-
-settings.test_db_alchemy_url = f"{settings.db_alchemy_driver}://{settings.test_db_user}:{settings.test_db_password}@\
-{settings.test_db_host}:{settings.test_db_port}/{settings.test_db_name}"
 
 print(f"DEVELOPMENT: {settings.development}")
 print(f"DEBUG_DATABASE: {settings.debug_database}")
