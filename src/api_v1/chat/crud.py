9315b8d43125763cd09aa45a825ab93c6aa1cceb
Artem <vebulogmetra@yandex.ru>
2023-11-22 22:58:39 +0300

    add crud for chat

--- src/api_v1/chat/crud.py
+++ src/api_v1/chat/crud.py
@@ -5,49 +5,61 @@ from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
-from src.api_v1.associates.models import UserChat
+from src.api_v1.associates.models import UserDialog
 from src.api_v1.base.utils import is_valid_uuid
-from src.api_v1.chat.models import Chat
-from src.api_v1.chat.schemas import AddUserToChat, ChatCreate
+from src.api_v1.chat.models import Dialog, Message
+from src.api_v1.chat.schemas import AddUserToDialog, DialogCreate, MessageCreate
 from src.utils.exceptions import custom_exc
 
 
-async def create_chat(db_session: AsyncSession, chat_data: ChatCreate) -> Chat:
-    chat = Chat(**chat_data.model_dump())
+async def create_dialog(db_session: AsyncSession, dialog_data: DialogCreate) -> Dialog:
+    chat = Dialog(**dialog_data.model_dump())
     db_session.add(chat)
     await db_session.commit()
     await db_session.refresh(chat)
     return chat
 
 
-async def add_user_to_chat(db_session: AsyncSession, data: AddUserToChat):
-    user_chat = UserChat(**data.model_dump())
-    db_session.add(user_chat)
+async def add_user_to_dialog(db_session: AsyncSession, data: AddUserToDialog):
+    user_dialog = UserDialog(**data.model_dump())
+    db_session.add(user_dialog)
     await db_session.commit()
 
 
-async def get_chats(db_session: AsyncSession, limit: int, offset: int) -> list[Chat]:
+async def get_dialogs(db_session: AsyncSession, limit: int, offset: int) -> list[Dialog]:
     stmt = (
-        select(Chat)
-        # .options(joinedload(Chat.participants))
+        select(Dialog)
+        .options(joinedload(Dialog.members), joinedload(Dialog.messages))
         .limit(limit)
         .offset(offset)
-        .order_by(Chat.created_at)
+        .order_by(Dialog.created_at)
     )
     result: Result = await db_session.execute(stmt)
-    chats: list[Chat] = result.scalars().unique()
-    if chats is None:
-        raise custom_exc.not_found(entity_name=f"{Chat.__name__}s")
-    return list(chats)
+    dialogs: list[Dialog] = result.scalars().unique()
+    if dialogs is None:
+        raise custom_exc.not_found(entity_name=f"{Dialog.__name__}s")
+    return list(dialogs)
 
 
-async def get_chat(db_session: AsyncSession, chat_id: UUID) -> Chat | None:
-    is_uuid: bool = is_valid_uuid(value=chat_id)
+async def get_dialog(db_session: AsyncSession, dialog_id: UUID) -> Dialog | None:
+    is_uuid: bool = is_valid_uuid(value=dialog_id)
     if is_uuid is False:
-        raise custom_exc.invalid_input(detail="chat id must by valid type UUID4")
-    stmt = select(Chat).options(joinedload(Chat.participants)).where(Chat.id == chat_id)
+        raise custom_exc.invalid_input(detail="dialog id must by valid type UUID4")
+    stmt = (
+        select(Dialog)
+        .options(joinedload(Dialog.members), joinedload(Dialog.messages))
+        .where(Dialog.id == dialog_id)
+    )
     result: Result = await db_session.execute(stmt)
-    chat = result.scalar()
-    if chat is None:
-        raise custom_exc.not_found(entity_name=Chat.__name__)
-    return chat
+    dialog = result.scalar()
+    if dialog is None:
+        raise custom_exc.not_found(entity_name=Dialog.__name__)
+    return dialog
+
+
+async def add_message(db_session: AsyncSession, message_data: MessageCreate) -> Message:
+    msg = Message(**message_data.model_dump())
+    db_session.add(msg)
+    await db_session.commit()
+    await db_session.refresh(msg)
+    return msg
--- src/api_v1/chat/views.py
+++ src/api_v1/chat/views.py
@@ -1,66 +1,104 @@
 from typing import Optional
 
-from fastapi import APIRouter, Depends, WebSocket
+from fastapi import APIRouter, Depends, WebSocket, WebSocketDisconnect
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api_v1.auth.schemas import TokenUserData
 from src.api_v1.auth.service import get_current_user
 from src.api_v1.base.schemas import StatusMsg
 from src.api_v1.chat import crud
-from src.api_v1.chat.schemas import AddUserToChat, ChatCreate
+from src.api_v1.chat.schemas import AddUserToDialog, DialogCreate, MessageCreate
 from src.utils.database import get_db
+from src.utils.websocket import ws_manager
 
 router = APIRouter()
 
 
 # @router.post("/create", response_model=ChatGet)
 @router.post("/create")
-async def create_chat_handler(
-    chat_data: ChatCreate,
+async def create_dialog_handler(
+    dialog_data: DialogCreate,
     session: AsyncSession = Depends(get_db),
+    current_user: TokenUserData = Depends(get_current_user),
 ):
-    return await crud.create_chat(db_session=session, chat_data=chat_data)
+    new_dialog = await crud.create_dialog(db_session=session, dialog_data=dialog_data)
+    # add creator to dialog memebers
+    data = AddUserToDialog(dialog_id=new_dialog.id, user_id=current_user.id)
+    await crud.add_user_to_dialog(db_session=session, data=data)
+    await session.refresh(new_dialog)
+    return new_dialog
 
 
 @router.post("/add_user", response_model=StatusMsg)
-async def add_user_to_chat_handler(
-    data: AddUserToChat,
+async def add_user_to_dialog_handler(
+    data: AddUserToDialog,
     session: AsyncSession = Depends(get_db),
     current_user: TokenUserData = Depends(get_current_user),
 ):
     if data.user_id is None:
         data.user_id = current_user.id
-    await crud.add_user_to_chat(db_session=session, data=data)
+    await crud.add_user_to_dialog(db_session=session, data=data)
     return StatusMsg(
         status="ok",
-        detail=f"User {data.user_id} added in chat {data.chat_id}",
+        detail=f"User {data.user_id} added in dialog {data.dialog_id}",
     )
 
 
 # @router.get("/chats", response_model=list[ChatGet])
-@router.get("/chats")
-async def get_chats_handler(
+@router.get("/dialogs")
+async def get_dialogs_handler(
     limit: Optional[int] = 100,
     offset: Optional[int] = 0,
     session: AsyncSession = Depends(get_db),
 ):
-    return await crud.get_chats(db_session=session, limit=limit, offset=offset)
+    return await crud.get_dialogs(db_session=session, limit=limit, offset=offset)
 
 
 # @router.get("/chat", response_model=ChatGet)
-@router.get("/chat")
-async def get_chat_by_id_handler(
-    chat_id: str,
+@router.get("/dialog")
+async def get_dialog_by_id_handler(
+    dialog_id: str,
     session: AsyncSession = Depends(get_db),
     current_user: TokenUserData = Depends(get_current_user),
 ):
-    chat = await crud.get_chat(db_session=session, chat_id=chat_id)
-    return chat
+    dialog = await crud.get_dialog(db_session=session, dialog_id=dialog_id)
+    return dialog
 
 
-@router.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+@router.post("/add_message")
+async def add_message_handler(
+    message_data: MessageCreate,
+    session: AsyncSession = Depends(get_db),
+    current_user: TokenUserData = Depends(get_current_user),
+):
+    if message_data.sender_id is None:
+        message_data.sender_id = current_user.id
+
+    await crud.add_message(db_session=session, message_data=message_data)
+    return StatusMsg(
+        status="ok",
+        detail=f"Message '{message_data.content}' \
+added in dialog {message_data.dialog_id}",
+    )
+
+
+@router.websocket("/ws/{dialog_id}/{user_id}")
+async def websocket_endpoint(
+    websocket: WebSocket,
+    dialog_id: str,
+    user_id: str,
+    session: AsyncSession = Depends(get_db),
+):
+    await ws_manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            # await ws_manager.send_personal_message(f"You wrote: {data}", websocket)
+            await ws_manager.broadcast(f"{data}")
+            message_data: MessageCreate = MessageCreate(
+                dialog_id=dialog_id, sender_id=user_id, content=data
+            )
+            await crud.add_message(db_session=session, message_data=message_data)
+    except WebSocketDisconnect:
+        ws_manager.disconnect(websocket)
+        await ws_manager.broadcast(f"Client #{user_id} left the chat")
